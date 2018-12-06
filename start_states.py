a2b7eb4c95c0d530935a14534fa707a1c6702618
Lina <lina.murady@gmail.com>
2018-12-06 16:43:20 +0100

    function added

--- start_states.py
+++ start_states.py
@@ -1,4 +1,39 @@
 import gym
+import argparse
+
+
+def get_start_state(env, env_name, start_n):
+    env.reset()
+    env_demo = {"FrozenLake-v0": [4, 8, 9, 13, 14], 
+        "MountainCarContinuous-v0":[0]*50 + [2]*30 + [0]*50 + [2]*60,
+        "Freeway-ramDeterministic-v0": [1]*43}
+
+    if env_name == "FrozenLake-v0":
+        states = env_demo[env_name]
+        env.env.s = states[start_n]
+        return env.env.s
+    elif env_name == "MountainCarContinuous-v0":
+        states = [] 
+        action_list = env_demo[env_name]
+        for action in action_list:
+            observation = env.step(action)
+            done = observation[2]
+            states.append(observation[0])
+            if done:
+                break
+        env.env.state = states[start]  
+        return env.env.state    
+    elif env_name == "Freeway-ramDeterministic-v0":
+        states = []
+        action_list = env_demo[env_name]
+        for action in action_list:
+            observation =  env.step(action)
+            done = observation[2]
+            states.append(observation[0])
+        env.env.state = states[start]   
+        return env.env.state    
+    return None 
+
 
 def main(args):
 
