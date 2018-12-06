61145bfbbb24373ddd820125f10b1f4659517762
Lina <lina.murady@gmail.com>
2018-12-06 16:42:19 +0100

    demonstration added

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import torch.nn.functional as F
 from torch import optim
 
 from policy import PolicyNetwork
+from start_states import get_start_state
+import utils 
 
 # FrozenLake-v0, MountainCar-v0, FreewayRAM-v0
 ENV = None
@@ -74,10 +76,23 @@ def run_iterations(args):
         action_size = 2
     model = PolicyNetwork(state_size, action_size)
     optimizer = optim.Adam(model.parameters(), args.learning_rate)
-
+    start_n = 4
     reward_per_iteration = []
     for i in range(args.max_iterations):
-        state = to_tensor(ENV.reset(), state_size)
+        # boolean for demo 
+        if not args.demo:
+            state = to_tensor(ENV.reset(), state_size)
+        else:
+            # start_n, nde state van demo pakken om als start state te gebruiken
+            # hoe deze te kiezen samen met max_iterations,  elke start state paar keer doen of 1x? 
+            start_state = get_start_state(ENV, args.env, start_n)
+            # probleem met ene environment ENV.env.s en andere ENV.env.state; misschien elegantere oplossing?
+            if args.env == "FrozenLake-v0": 
+                ENV.env.s = start_state
+                state = to_tensor(ENV.env.s, state_size)
+            else:
+                ENV.env.state = start_state
+                state = to_tensor(ENV.env.state, state_size)
         reward_per_episode = []
         episode_loss = 0
         for step in range(args.max_steps):
@@ -118,14 +133,12 @@ def plot_rewards(rewards):
     plt.ylabel("Reward")
     plt.show()
 
-
 def main(args):
     load_environment(args.env)
     reward_per_iteration = run_iterations(args)
     ENV.close()
 
     plot_rewards(reward_per_iteration)
-
 
 
 if __name__ == '__main__':
@@ -138,6 +151,7 @@ if __name__ == '__main__':
     parser.add_argument('--learning-rate', type=float, default=0.0001, help="Learning rate")
     parser.add_argument('--discount-factor', type=float, default=0.8, help="Discount factor")
     parser.add_argument('--print-every', type=int, default=10, help="Print status every x iteration")
+    parser.add_argument('--demo', default=True, type=utils.boolean_string, help="Whether to use demonstration or not")
     args = parser.parse_args()
 
     main(args)
