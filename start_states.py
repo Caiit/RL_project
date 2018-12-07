6522843473201458687e9649554f3baa48323a6f
ukhuranauva <rubyurja@msn.com>
2018-12-07 20:29:02 +0100

    got states for other environments

--- start_states.py
+++ start_states.py
@@ -4,7 +4,7 @@ import argparse
 
 def get_start_state(env, env_name, start_n):
     env.reset()
-    env_demo = {"FrozenLake-v0": [4, 8, 9, 13, 14], 
+    env_demo = {"FrozenLake-v0": [4, 8, 9, 13, 14],
         "MountainCarContinuous-v0":[0]*50 + [2]*30 + [0]*50 + [2]*60,
         "Freeway-ramDeterministic-v0": [1]*43}
 
@@ -13,26 +13,14 @@ def get_start_state(env, env_name, start_n):
         env.env.s = states[start_n]
         return env.env.s
     elif env_name == "MountainCarContinuous-v0":
-        states = [] 
-        action_list = env_demo[env_name]
-        for action in action_list:
-            observation = env.step(action)
-            done = observation[2]
-            states.append(observation[0])
-            if done:
-                break
-        env.env.state = states[start]  
-        return env.env.state    
+        states = np.load('states_mountaincar.npy')
+        env.env.state = states[start]
+        return env.env.state
     elif env_name == "Freeway-ramDeterministic-v0":
-        states = []
-        action_list = env_demo[env_name]
-        for action in action_list:
-            observation =  env.step(action)
-            done = observation[2]
-            states.append(observation[0])
-        env.env.state = states[start]   
-        return env.env.state    
-    return None 
+        states = np.load('states_freeway.npy')
+        restored = env.env.restore_store(states[start])
+        return restored
+    return None
 
 
 def main(args):
